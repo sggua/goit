5846f0231c42376b9cc6c648fdbf036da105e261
sergiy <m@sgg.im>
2016-08-19 21:03:38 +0300

    module01 hw + kotlin

--- JavaCoreGoIt/z_kotlin/src/module01/SecondClass.kt
+++ JavaCoreGoIt/z_kotlin/src/module01/SecondClass.kt
@@ -1,3 +1,5 @@
+package module01
+
 fun main(args: Array<String>) {
     println("My second class on Kotlin")
 }
--- JavaCoreGoIt/z_kotlin/src/module01/ThirdClass.kt
+++ JavaCoreGoIt/z_kotlin/src/module01/ThirdClass.kt
@@ -1,3 +1,5 @@
+package module01
+
 fun main(args: Array<String>) {
     println("My third class on Kotlin")
 }
