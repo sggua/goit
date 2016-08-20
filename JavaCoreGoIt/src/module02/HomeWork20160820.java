83cea952bc4f3b11c7a4a15b582f5fb7f7dc6934
sergiy <m@sgg.im>
2016-08-21 02:43:54 +0300

    module02 hw checkstyle

--- JavaCoreGoIt/src/module02/HomeWork20160820.java
+++ JavaCoreGoIt/src/module02/HomeWork20160820.java
@@ -1,50 +1,65 @@
 package module02;
 
-public class HomeWork20160820 {
+class HomeWork20160820 {
 
-    static int getSum(int[] nums){
-        int sum=0;
-        for (int num:nums ) sum += num;
+    private static int getSum(int[] numbers) {
+        int sum = 0;
+        for (int num:numbers) {
+            sum += num;
+        }
         return sum;
     }
 
-    static int getMin(int[] nums) {
-        int min=nums[0];
-        for (int num:nums ) if (num<min) min=num;
+    private static int getMin(int[] numbers) {
+        int min = numbers[0];
+        for (int num:numbers) {
+            if (num < min) {
+                min = num;
+            }
+        }
         return min;
     }
 
-    static int getMax(int[] nums) {
-        int max=nums[0];
-        for (int num:nums ) if (num>max) max=num;
+    private static int getMax(int[] numbers) {
+        int max = numbers[0];
+        for (int num:numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
         return max;
     }
 
-    static int getMaxPositive(int[] nums) {
-        int maxPos=nums[0];
-        for (int num:nums ) if (num>0 && num>maxPos) maxPos=num;
+    private static int getMaxPositive(int[] numbers) {
+        int maxPos = numbers[0];
+        for (int num:numbers) {
+            if (num > 0 && num > maxPos) {
+                maxPos = num;
+            }
+        }
         return maxPos;
     }
 
-    static int getMult(int[] nums) {
-        int mult=1;
-        for (int num:nums ) mult*=num;
-        return mult;
+    private static int getMultiplied(int[] numbers) {
+        int multiplied = 1;
+        for (int num:numbers) {
+            multiplied *= num;
+        }
+        return multiplied;
     }
 
-    static int getModFirstByLast(int[] nums) {
-        return nums[0] % nums[nums.length-1];
+    private static int getModFirstByLast(int[] numbers) {
+        return numbers[0] % numbers[numbers.length - 1];
     }
 
 
     public static void main(String[] args) {
-        int[] numbers = {21,4,5,6,7,
-                        1,17,12,11,6};
+        int[] numbers = {21, 4, 5, 6, 7, 1, 17, 12, 11, 6 };
         System.out.println("sum: " + getSum(numbers));
         System.out.println("min: " + getMin(numbers));
         System.out.println("max: " + getMax(numbers));
         System.out.println("maxPositive: " + getMaxPositive(numbers));
-        System.out.println("mult: " + getMult(numbers));
+        System.out.println("multiplied: " + getMultiplied(numbers));
         System.out.println("mod: " + getModFirstByLast(numbers));
 
     }
