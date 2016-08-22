383c88ea5e338e62a426e8a2adcb05f82ef34fb2
sergiy <m@sgg.im>
2016-08-22 12:47:11 +0300

    module02 hw updated task

--- JavaCoreGoIt/src/module02/HomeWork20160820.java
+++ JavaCoreGoIt/src/module02/HomeWork20160820.java
@@ -4,7 +4,15 @@ class HomeWork20160820 {
 
     private static int getSum(int[] numbers) {
         int sum = 0;
-        for (int num:numbers) {
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static double getSum(double[] numbers) {
+        double sum = 0;
+        for (double num : numbers) {
             sum += num;
         }
         return sum;
@@ -12,7 +20,17 @@ class HomeWork20160820 {
 
     private static int getMin(int[] numbers) {
         int min = numbers[0];
-        for (int num:numbers) {
+        for (int num : numbers) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    private static double getMin(double[] numbers) {
+        double min = numbers[0];
+        for (double num : numbers) {
             if (num < min) {
                 min = num;
             }
@@ -22,7 +40,7 @@ class HomeWork20160820 {
 
     private static int getMax(int[] numbers) {
         int max = numbers[0];
-        for (int num:numbers) {
+        for (int num : numbers) {
             if (num > max) {
                 max = num;
             }
@@ -30,9 +48,51 @@ class HomeWork20160820 {
         return max;
     }
 
+    private static double getMax(double[] numbers) {
+        double max = numbers[0];
+        for (double num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    private static int getSecondLargest(int[] numbers) {
+        int max = getMax(numbers);
+        int secondMax = Integer.MIN_VALUE;
+        for (int num : numbers) {
+            if (num > secondMax && num < max) {
+                secondMax = num;
+            }
+        }
+        return secondMax;
+    }
+
+    private static double getSecondLargest(double[] numbers) {
+        double max = getMax(numbers);
+        double secondMax = Double.MIN_VALUE;
+        for (double num : numbers) {
+            if (num > secondMax && num < max) {
+                secondMax = num;
+            }
+        }
+        return secondMax;
+    }
+
     private static int getMaxPositive(int[] numbers) {
-        int maxPos = numbers[0];
-        for (int num:numbers) {
+        int maxPos = -1;
+        for (int num : numbers) {
+            if (num > 0 && num > maxPos) {
+                maxPos = num;
+            }
+        }
+        return maxPos;
+    }
+
+    private static double getMaxPositive(double[] numbers) {
+        double maxPos = -1;
+        for (double num : numbers) {
             if (num > 0 && num > maxPos) {
                 maxPos = num;
             }
@@ -42,7 +102,15 @@ class HomeWork20160820 {
 
     private static int getMultiplied(int[] numbers) {
         int multiplied = 1;
-        for (int num:numbers) {
+        for (int num : numbers) {
+            multiplied *= num;
+        }
+        return multiplied;
+    }
+
+    private static double getMultiplied(double[] numbers) {
+        double multiplied = 1;
+        for (double num : numbers) {
             multiplied *= num;
         }
         return multiplied;
@@ -52,15 +120,30 @@ class HomeWork20160820 {
         return numbers[0] % numbers[numbers.length - 1];
     }
 
+    private static double getModFirstByLast(double[] numbers) {
+        return numbers[0] % numbers[numbers.length - 1];
+    }
+
 
     public static void main(String[] args) {
-        int[] numbers = {21, 4, 5, 6, 7, 1, 17, 12, 11, 6 };
-        System.out.println("sum: " + getSum(numbers));
-        System.out.println("min: " + getMin(numbers));
-        System.out.println("max: " + getMax(numbers));
-        System.out.println("maxPositive: " + getMaxPositive(numbers));
-        System.out.println("multiplied: " + getMultiplied(numbers));
-        System.out.println("mod: " + getModFirstByLast(numbers));
+        int[] numbersInt = {21, -4, 5, -6, 7, -1, 17, 1, 11, 6};
+        double[] numbersDouble = {2.1, -0.4, 5.7, -3.6, 7.6, -1.0, 17.3, 0.1, 11.9, 6.3};
+        System.out.println("INTEGER PART");
+        System.out.println("sum: " + getSum(numbersInt));
+        System.out.println("min: " + getMin(numbersInt));
+        System.out.println("max: " + getMax(numbersInt));
+        System.out.println("maxPositive: " + getMaxPositive(numbersInt));
+        System.out.println("multiplied: " + getMultiplied(numbersInt));
+        System.out.println("mod: " + getModFirstByLast(numbersInt));
+        System.out.println("secondLargest: " + getSecondLargest(numbersInt));
+        System.out.println("\nDOUBLE PART");
+        System.out.println("sum: " + getSum(numbersDouble));
+        System.out.println("min: " + getMin(numbersDouble));
+        System.out.println("max: " + getMax(numbersDouble));
+        System.out.println("maxPositive: " + getMaxPositive(numbersDouble));
+        System.out.println("multiplied: " + getMultiplied(numbersDouble));
+        System.out.println("mod: " + getModFirstByLast(numbersDouble));
+        System.out.println("secondLargest: " + getSecondLargest(numbersDouble));
 
     }
 }
