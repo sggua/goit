123ad00276f82da8ef6364242328abea1c675058
sergiy <m@sgg.im>
2016-08-23 22:41:14 +0300

    module02 hw task part 2.4 kotlin

--- JavaCoreGoIt/z_kotlin/src/module02/HomeWork24by20160823.kt
+++ JavaCoreGoIt/z_kotlin/src/module02/HomeWork24by20160823.kt
@@ -5,29 +5,39 @@ import java.util.*
 private val ownerNames = arrayOf("Alex", "Bob", "Caren", "Dude", "Eagle", "Foo", "Gone", "Hook", "Irene", "Joe")
 private val balances = doubleArrayOf(234.1, 230.5, 145.6, 51.0, 45.6, 567.4, 34.7, 876.53, 234.5, 56.7)
 
-private fun fund(fund: Double, name: String, balances: DoubleArray, names: Array<String>): String {
-    val nameIndex = getNameIndex(names, name)
-    if (nameIndex >= 0) {
-        if (fund > 0) {
-            return "$name " + fundBalance(balances, nameIndex, fund)
+
+private fun fundResult(ownerName: String, fund: Double): String {
+    val resultFund = fundBalance(ownerName, fund)
+    if (fund > 0) {
+        if (resultFund >= 0) {
+            return "$ownerName $resultFund"
         } else {
-            return "$name's balance was not funded due to incorrect fund amount ($fund)"
+            return "$ownerName's balance probably not exists"
         }
     } else {
-        return "$name not found"
+        return "$ownerName's balance was not funded due to incorrect fund amount ($fund)"
     }
 }
 
-private fun getNameIndex(names: Array<String>, name: String): Int {
-    for (i in names.indices) {
-        if (names[i] == name) {
+private fun fundBalance(ownerName: String, fund: Double): Double {
+    val nameIndex = getNameIndex(ownerName)
+    if (nameIndex >= 0 && fund > 0) {
+        return fundBalanceByIndex(nameIndex, fund)
+    } else {
+        return -1.0
+    }
+}
+
+private fun getNameIndex(ownerName: String): Int {
+    for (i in ownerNames.indices) {
+        if (ownerNames[i] == ownerName) {
             return i
         }
     }
     return -1 // if not found
 }
 
-private fun fundBalance(balances: DoubleArray, index: Int, fund: Double): Double {
+private fun fundBalanceByIndex(index: Int, fund: Double): Double {
     balances[index] += fund
     return balances[index]
 }
@@ -37,11 +47,11 @@ fun main(args: Array<String>) {
     println(Arrays.toString(ownerNames))
     println(Arrays.toString(balances))
 
-    println(fund(50.0, "Alex", balances, ownerNames))
-    println(fund(50.0, "Caren", balances, ownerNames))
-    println(fund(50.0, "Daniel", balances, ownerNames))
-    println(fund(-20.5, "Dude", balances, ownerNames))
-    println(fund(50.0, "Hook", balances, ownerNames))
+    println(fundResult("Alex", 50.0))
+    println(fundResult("Caren", 0.0))
+    println(fundResult("Daniel", 50.0))
+    println(fundResult("Dude", -20.5))
+    println(fundResult("Hook", 50.0))
 
     println(Arrays.toString(balances))
 }
