4d81412950c4957d011eacd4d0b0ba6bcf90749f
sergiy <m@sgg.im>
2016-08-23 23:09:29 +0300

    module02 practice

--- JavaCoreGoIt/src/module02/practice20160820/Task01.java
+++ JavaCoreGoIt/src/module02/practice20160820/Task01.java
@@ -2,23 +2,27 @@ package module02.practice20160820;
 
 class Task01 {
 
-    private static int balancesSum(int[] ints){
-        int sum=0;
+    static String[] ownerNames = {"Alex", "Bob", "Caren", "Dude", "Eagle", "Foo", "Gone", "Hook", "Irene", "Joe"};
+    static int[] balances = {2341, 2305, 1456, 239, 456, 5674, 347, 87653, 2345, 567};
+    static int[] balances2 = {20341, 205, 2456, 1239, 4056, 574, 47, 1653, 235, 5267};
+
+    private static int balancesSum(int[] ints) {
+        int sum = 0;
         for (int anInt : ints) {
             if (anInt > 1000 && anInt < 5000) {
                 sum += anInt;
             }
         }
-        return(sum);
+        return (sum);
     }
 
-    private static String[] getOwnerNames(int[] balances, String[] names){
+    private static String[] getOwnerNames(int[] balances, String[] names) {
         int len = balances.length;
-        int resCount=0;
-        String[] result1 =new String[len];
-        for (int i=0; i<len ; i++) {
+        int resCount = 0;
+        String[] result1 = new String[len];
+        for (int i = 0; i < len; i++) {
             if (balances[i] > 1000 && balances[i] < 5000) {
-                result1[resCount++]=names[i];
+                result1[resCount++] = names[i];
             }
         }
 
@@ -31,20 +35,17 @@ class Task01 {
     public static void main(String[] args) {
 //        int variable = 4;
 
-        String [] ownerNames = {"Alex","Bob","Caren","Dude","Eagle","Foo","Gone","Hook","Irene","Joe" };
-        int[] balances = {  2341,2305,  1456, 239,456, 5674, 347, 87653,2345,567};
-        int[] balances2 = {  20341,205,  2456, 1239,4056, 574, 47, 1653,235,5267};
 
 /////////////////////////////////////////////////////////////
-        int sum=0;
-        for (int i=0; i<balances.length ; i++) {
-            if (balances[i]>1000 && balances[i]<5000) {
-                sum+=balances[i];
+        int sum = 0;
+        for (int i = 0; i < balances.length; i++) {
+            if (balances[i] > 1000 && balances[i] < 5000) {
+                sum += balances[i];
             }
         }
         System.out.println(sum);
 /////////////////////////////////////////////////////////////
-        sum=0;
+        sum = 0;
         for (int balance : balances) {
             if (balance > 1000 && balance < 5000) {
                 sum += balance;
@@ -59,7 +60,7 @@ class Task01 {
 /////////////////////////////////////////////////////////////
 
 
-        for (String n: getOwnerNames(balances, ownerNames) ) {
+        for (String n : getOwnerNames(balances, ownerNames)) {
             System.out.println(n);
         }
 
