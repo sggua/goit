1c8a871fc6fc8680fcc2fbf8fcb6bfac47b024b4
sergiy <m@sgg.im>
2016-08-23 22:31:56 +0300

    module02 hw task part 2.4

--- JavaCoreGoIt/src/module02/HomeWork24by20160823.java
+++ JavaCoreGoIt/src/module02/HomeWork24by20160823.java
@@ -9,29 +9,38 @@ public class HomeWork24by20160823 {
     };
     private static double[] balances = {234.1, 230.5, 145.6, 51.0, 45.6, 567.4, 34.7, 876.53, 234.5, 56.7};
 
-    private static String fund(double fund, String name, double[] balances, String[] names) {
-        int nameIndex = getNameIndex(names, name);
-        if (nameIndex >= 0) {
-            if (fund > 0) {
-                return name + " " + fundBalance(balances, nameIndex, fund);
-            } else {
-                return name + "'s balance was not funded due to incorrect fund amount (" + fund + ")";
+    private static String fundResult(String ownerName, double fund) {
+        double resultFund = fundBalance(ownerName, fund);
+        if (fund > 0) {
+            if (resultFund >= 0) {
+                return ownerName + " " + resultFund;
+            } else{
+                return ownerName + "'s balance probably not exists";
             }
         } else {
-            return name + " not found";
+            return ownerName + "'s balance was not funded due to incorrect fund amount (" + fund + ")";
         }
     }
 
-    private static int getNameIndex(String[] names, String name) {
+    private static double fundBalance(String ownerName, double fund) {
+        int nameIndex = getNameIndex(ownerNames, ownerName);
+        if (nameIndex >= 0 && fund > 0) {
+            return fundBalanceByIndex(nameIndex, fund);
+        } else {
+            return -1;
+        }
+    }
+
+    private static int getNameIndex(String[] names, String ownerName) {
         for (int i = 0; i < names.length; i++) {
-            if (names[i].equals(name)) {
+            if (names[i].equals(ownerName)) {
                 return i;
             }
         }
         return -1; // if not found
     }
 
-    private static double fundBalance(double[] balances, int index, double fund) {
+    private static double fundBalanceByIndex(int index, double fund) {
         balances[index] += fund;
         return balances[index];
     }
@@ -42,11 +51,11 @@ public class HomeWork24by20160823 {
         System.out.println(Arrays.toString(ownerNames));
         System.out.println(Arrays.toString(balances));
 
-        System.out.println(fund(50, "Alex", balances, ownerNames));
-        System.out.println(fund(50, "Caren", balances, ownerNames));
-        System.out.println(fund(50, "Daniel", balances, ownerNames));
-        System.out.println(fund(-20.5, "Dude", balances, ownerNames));
-        System.out.println(fund(50, "Hook", balances, ownerNames));
+        System.out.println(fundResult("Alex", 50));
+        System.out.println(fundResult("Caren", 0));
+        System.out.println(fundResult("Daniel", 50));
+        System.out.println(fundResult("Dude", -20.5));
+        System.out.println(fundResult("Hook", 50));
 
         System.out.println(Arrays.toString(balances));
 
