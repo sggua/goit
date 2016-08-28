4277718c72443961b187afe1a547dee7d17facdd
sergiy <m@sgg.im>
2016-08-28 04:12:11 +0300

    module03 hw task 3.4

---
+++ JavaCoreGoIt/src/module03/HomeWork34/HomeWork34On20160828.java
@@ -0,0 +1,27 @@
+package module03.HomeWork34;
+
+public class HomeWork34On20160828 {
+
+    public static void main(String[] args) {
+        User user = new User("Den", 28000, 3, "Code Practice", 1500, "USD");
+
+        System.out.println(user.getBalance());
+
+        user.paySalary();
+        System.out.println(user.getBalance());
+
+        user.withdraw(1000);
+        System.out.println(user.getBalance());
+        user.withdraw(500);
+        System.out.println(user.getBalance());
+
+        System.out.println(user.companyNameLength());
+
+        System.out.println(user.getMonthsOfEmployment());
+        user.monthIncreaser(5);
+        System.out.println(user.getMonthsOfEmployment());
+
+
+    }
+
+}
---
+++ JavaCoreGoIt/src/module03/HomeWork34/User.java
@@ -0,0 +1,100 @@
+package module03.HomeWork34;
+
+public class User {
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
+
+    public User() {
+        this("", 0, 0, "", 0, "");
+    }
+
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    /*    Write methods:
+
+        void paySalary() -  that add salary to the balance of the user
+        withdraw(int summ) - takes money from the balance with 5% commision if summ < 1000 and 10% commision in other cases
+        companyNameLenfht - calculates name of the company name
+        monthIncreaser(int addMonth) - increase monthsOfEmployment by addMonth
+      */
+
+    void paySalary() {
+        this.setBalance(this.getBalance() + this.getSalary());
+    }
+
+    void withdraw(int sum) {
+        if (sum < 1000) {
+            this.setBalance(this.getBalance() - sum * 5 / 100);
+        } else {
+            this.setBalance(this.getBalance() - sum * 10 / 100);
+        }
+    }
+
+    int companyNameLength() {
+        return this.getCompanyName().length();
+    }
+
+    void monthIncreaser(int addMonth) {
+        this.setMonthsOfEmployment(this.getMonthsOfEmployment() + addMonth);
+    }
+
+}
