5feaf677fe5686db6b5ac0741f3456d23f219852
sergiy <m@sgg.im>
2016-09-02 09:31:11 +0300

    module03 offline hub

--- JavaCoreGoIt/src/module03/OfflineOn20160831/Task02/_Launcher.java
+++ JavaCoreGoIt/src/module03/OfflineOn20160831/Task02/_Launcher.java
@@ -4,8 +4,8 @@ public class _Launcher {
     public static void main(String[] args) {
         Converter conv = new Converter(26.51);
 
-        System.out.println("1000 uah \t = \t"+conv.uah2Usd(1300));
-        System.out.println("1000 usd \t = \t"+conv.usd2Uah(1300));
+        System.out.println("100 uah \t = \t"+conv.uah2Usd(100));
+        System.out.println("100 usd \t = \t"+conv.usd2Uah(100));
 
     }
 }
