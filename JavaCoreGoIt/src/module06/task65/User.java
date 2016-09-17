fc3b14494e2c34f42fad732c80036bdc0e3348e4
sergiy <m@sgg.im>
2016-09-17 12:04:42 +0300

    module06 hw task 6.5

---
+++ JavaCoreGoIt/src/module06/task65/User.java
@@ -0,0 +1,18 @@
+package module06.task65;
+
+public class User {
+    long id;
+    String firstName;
+    String lastName;
+    int salary;
+    int balance;
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+
+}
